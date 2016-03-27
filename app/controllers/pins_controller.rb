f943d807fa1b47e9d9fe8b7e6194340c853220b0
Bu7ch <dutchr@live.fr>
2016-03-27 23:42:24 +0200

    rename file & set up controller Pin

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,10 @@
 class PinsController < ApplicationController
+before_action :find_pin,  only: [:show, :edit, :update, :destroy]
+
   def index
+
+  end
+  def show
 
   end
   def new
@@ -10,12 +15,16 @@ class PinsController < ApplicationController
 
     if @pin.save
       redirect_to @pin, notice: "Succesfully saved"
-
+    else
+      render 'new'
     end
   end
 
-private
-def pin_params
-  params.require(:pin).permit(:title, :description)
-end
+  private
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
 end
